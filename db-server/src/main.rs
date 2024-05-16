d54ac600fd31b13d261436e9eee10040cbbf408f
Preston Garrison III <preston.garrison3@gmail.com>
2024-05-16 14:56:26 -0400

    fix: use non async for sending

--- db-server/src/main.rs
+++ db-server/src/main.rs
@@ -1,11 +1,12 @@
 use std::{
     fs::{File, OpenOptions},
     io::{self, Read, Write},
+    sync::mpsc::Sender,
 };
 
 use anyhow::anyhow;
 use clap::Parser;
-use db_link::commands::{Packet, MAX_PACKET_SIZE};
+use db_link::commands::{Packet, PayloadBuf, MAX_PACKET_SIZE};
 
 #[derive(Parser, Default)]
 #[command(version, about, long_about = None)]
@@ -20,23 +21,31 @@ fn main() -> Result<(), anyhow::Error> {
         .read(true)
         .write(true)
         .open(args.serial_port_path)?;
-    let mut buffer = [0u8; MAX_PACKET_SIZE];
-    let packet = Packet::Echo(b"Hello There");
-    serial.write_all(packet.serialize(&mut buffer))?;
-    let mut parser = db_link::parser::Parser::new();
-    //read untill we get a packet or error
-    loop {
-        let mut read_buffer = [0u8; MAX_PACKET_SIZE];
-        serial.read(&mut read_buffer)?;
-        match parser.parse(&read_buffer) {
-            Ok(packet) => {
-                println!("Got: {packet:?}");
-                break;
+    let packets = [
+        Packet::Echo(PayloadBuf::from_slice(b"bye").unwrap()).serialize(),
+        Packet::GetParam(PayloadBuf::from_slice(b"VERSION").unwrap()).serialize(),
+    ];
+    for i in 0..2 {
+        serial.write_all(&packets[i])?;
+        let mut parser = db_link::parser::Parser::new();
+        //read untill we get a packet or error
+        loop {
+            let mut read_buffer = [0u8; MAX_PACKET_SIZE];
+            let bytes = serial.read(&mut read_buffer)?;
+            if bytes > 0 {
+                match parser.parse(&read_buffer) {
+                    Ok(packet) => {
+                        println!("Got: {packet:?}");
+                        break;
+                    }
+                    Err(db_link::parser::Error::InvalidVersion) => {
+                        return Err(anyhow!("Invalid protocol version"));
+                    }
+                    Err(_) => {
+                        println!("{}", String::from_utf8_lossy(&read_buffer[..bytes]));
+                    }
+                }
             }
-            Err(db_link::parser::Error::InvalidVersion) => {
-                return Err(anyhow!("Invalid protocol version"));
-            }
-            Err(_) => {}
         }
     }
     Ok(())
--- desk-display-bare-metal/src/main.rs
+++ desk-display-bare-metal/src/main.rs
@@ -1,32 +1,58 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
 use core::ptr::addr_of_mut;
 
-use db_link::commands::MAX_PACKET_SIZE;
-use db_link::{commands::Packet, parser::Parser};
+use db_link::commands::{Command, MAX_PACKET_SIZE, MAX_PAYLOAD_SIZE};
+use db_link::{
+    commands::{Packet, PayloadBuf, ResponsePayload},
+    parser::Parser,
+};
 use embassy_executor::Spawner;
 use embassy_sync::signal::Signal;
-use embedded_io::Write;
+use embassy_time::Timer;
+use embedded_io::Write as EIOWrite;
 use esp_backtrace as _;
+use esp_hal::rmt::Rmt;
 use esp_hal::{
     clock::ClockControl,
     embassy,
+    gpio::IO,
     peripherals::Peripherals,
     prelude::*,
     timer::TimerGroup,
     usb_serial_jtag::{UsbSerialJtag, UsbSerialJtagRx, UsbSerialJtagTx},
     Async,
 };
+use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
 use heapless::spsc::{Consumer, Producer, Queue};
+use log::info;
+use smart_leds::hsv::Hsv;
+use smart_leds::{brightness, gamma, hsv::hsv2rgb, SmartLedsWrite};
 
-const MAX_BUFFER_SIZE: usize = 512;
+const QUEUE_SIZE: usize = 4096;
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 /// Handle packets, return vector should be sent back
 fn handle_packet(packet: Packet) -> heapless::Vec<u8, MAX_PACKET_SIZE> {
     match packet {
-        Packet::Echo(_) => packet.serialize_heapless_vec(),
-        _ => todo!(),
+        Packet::Echo(_) => packet.serialize(),
+        Packet::GetParam(param) => match param.as_slice() {
+            b"VERSION" => {
+                Packet::Response(PayloadBuf::from_slice(VERSION.as_bytes()).unwrap()).serialize()
+            }
+            _ => {
+                let mut buf = heapless::Vec::<u8, MAX_PAYLOAD_SIZE>::new();
+                _ = buf.write_str("unknown param");
+                Packet::Error(buf).serialize()
+            }
+        },
+        _ => {
+            let mut buf = heapless::Vec::<u8, MAX_PAYLOAD_SIZE>::new();
+            _ = buf.write_str("unknown command");
+            Packet::Error(buf).serialize()
+        }
     }
 }
 
@@ -34,27 +60,30 @@ fn handle_packet(packet: Packet) -> heapless::Vec<u8, MAX_PACKET_SIZE> {
 async fn writer(
     mut tx: UsbSerialJtagTx<'static, Async>,
     signal: &'static Signal<embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex, usize>,
-    mut fifo: Consumer<'static, u8, 1024>,
+    mut fifo: Consumer<'static, u8, QUEUE_SIZE>,
 ) {
     let mut parser = Parser::new();
+
     loop {
-        match fifo.dequeue() {
-            Some(byte) => {
-                match parser.parse(&[byte]) {
-                    Ok(packet) => {
-                        //TODO: I asumme this should only write the len of the vec but should check
-                        //this
-                        let buf = handle_packet(packet);
-                        tx.write_all(&buf).unwrap();
-                        embedded_io_async::Write::flush(&mut tx).await.unwrap();
-                    }
-                    Err(db_link::parser::Error::InvalidVersion) => {
-                        log::error!("Received invalid version");
-                    }
-                    Err(_) => {}
+        // info!("P Waiting");
+        let _len = signal.wait().await;
+        signal.reset();
+        // info!("P Got {len}bytes");
+        while let Some(byte) = fifo.dequeue() {
+            match parser.parse(&[byte]) {
+                Ok(packet) => {
+                    //TODO: I asumme this should only write the len of the vec but should check
+                    //this
+                    let buf = handle_packet(packet);
+                    tx.write_all(&buf).unwrap();
+                    //embedded_io_async::Write::flush(&mut tx).await.unwrap();
+                    // info!("P Wrote Packet");
                 }
+                Err(db_link::parser::Error::InvalidVersion) => {
+                    log::error!("P Received invalid version");
+                }
+                Err(_) => {}
             }
-            None => _ = signal.wait().await,
         }
     }
 }
@@ -63,16 +92,22 @@ async fn writer(
 async fn reader(
     mut rx: UsbSerialJtagRx<'static, Async>,
     signal: &'static Signal<embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex, usize>,
-    mut fifo: Producer<'static, u8, 1024>,
+    mut fifo: Producer<'static, u8, QUEUE_SIZE>,
 ) {
-    let mut rbuf = [0u8; MAX_BUFFER_SIZE];
+    let mut rbuf = [0u8; 512];
     loop {
+        // info!("R reading");
         let r = embedded_io_async::Read::read(&mut rx, &mut rbuf).await;
         match r {
             Ok(len) => {
                 for b in &rbuf[..len] {
-                    fifo.enqueue(*b).unwrap();
+                    while let Err(_) = fifo.enqueue(*b) {
+                        //fifo full, try to give parser time to process
+                        // info!("R fifo full");
+                        Timer::after_millis(20).await;
+                    }
                 }
+                // info!("R signaling");
                 signal.signal(len);
             }
             Err(e) => esp_println::println!("RX Error: {:?}", e),
@@ -83,6 +118,7 @@ async fn reader(
 #[main]
 async fn main(spawner: Spawner) {
     let peripherals = Peripherals::take();
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let system = peripherals.SYSTEM.split();
 
     let clocks = ClockControl::max(system.clock_control).freeze();
@@ -92,8 +128,8 @@ async fn main(spawner: Spawner) {
     let (tx, rx) = UsbSerialJtag::new_async(peripherals.USB_DEVICE).split();
     esp_println::logger::init_logger_from_env();
 
-    let fifo: &'static mut Queue<u8, 1024> = {
-        static mut Q: Queue<u8, 1024> = Queue::new();
+    let fifo: &'static mut Queue<u8, QUEUE_SIZE> = {
+        static mut Q: Queue<u8, QUEUE_SIZE> = Queue::new();
         unsafe { &mut *addr_of_mut!(Q) }
     };
     static DATA_SIGNAL: Signal<embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex, usize> =
@@ -102,5 +138,31 @@ async fn main(spawner: Spawner) {
     let (producer, consumer) = fifo.split();
 
     spawner.spawn(reader(rx, &DATA_SIGNAL, producer)).unwrap();
-    spawner.spawn(writer(tx, &DATA_SIGNAL, consumer)).unwrap()
+    spawner.spawn(writer(tx, &DATA_SIGNAL, consumer)).unwrap();
+
+    let rmt = Rmt::new(peripherals.RMT, 80.MHz(), &clocks, None).unwrap();
+    let rmt_buffer = smartLedBuffer!(1);
+    let mut led = SmartLedsAdapter::new(rmt.channel0, io.pins.gpio38, rmt_buffer, &clocks);
+
+    let mut color = Hsv {
+        hue: 0,
+        sat: 255,
+        val: 255,
+    };
+    let mut data;
+    loop {
+        // Iterate over the rainbow!
+        for hue in 0..=255 {
+            color.hue = hue;
+            // Convert from the HSV color space (where we can easily transition from one
+            // color to the other) to the RGB color space that we can then send to the LED
+            data = [hsv2rgb(color)];
+            // When sending to the LED, we do a gamma correction first (see smart_leds
+            // documentation for details) and then limit the brightness to 10 out of 255 so
+            // that the output it's not too bright.
+            led.write(brightness(gamma(data.iter().cloned()), 10))
+                .unwrap();
+            Timer::after_millis(20).await;
+        }
+    }
 }
